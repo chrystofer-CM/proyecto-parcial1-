821c1bf33ab22202cb4aeb9472c5b9e1bf3b6b4c
chrystofer29 <132796469+chrystofer29@users.noreply.github.com>
2025-02-28 10:58:18 -0600

    Proyecto Desarrollo Java Final Parcial 1

---
+++ src/main/java/com/mycompany/proyectoparcial1/Proyectoparcial1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoparcial1;
+
+/**
+ *
+ * @author ACER
+ */
+public class Proyectoparcial1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
